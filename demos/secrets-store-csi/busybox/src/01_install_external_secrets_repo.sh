104596d9245b701a3100dd50abdcaedfe8096a8d
Nick Png <nkpng2k@gmail.com>
2025-09-10 08:56:46 -0400

    move vault specific install scripts to utils dir

--- demos/secrets-store-csi/busybox/src/01_install_external_secrets_repo.sh
+++ demos/secrets-store-csi/busybox/src/01_install_external_secrets_repo.sh
@@ -2,13 +2,15 @@
 
 # Load in variables from varibles.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DEMO_SRC_DIR="secrets-store-csi/busybox/src"
+UTILS_DIR=$(sed "s|$DEMO_SRC_DIR|utils|g" <<< "$SCRIPT_DIR")
+source $UTILS_DIR/vault.sh
 source $SCRIPT_DIR/variables.sh
 
 echo "Running helm install scripts for $EXTERNAL_SECRETS_REPO"
 
 if [[ $EXTERNAL_SECRETS_REPO == "vault" ]]; then
-  helm repo add $HASHICORP_HELM_REPO $HASHICORP_HELM_REPO_URL
-  helm repo update
+  install_vault_helm_repo
 else
   echo "Did not recognize repo: $EXTERNAL_SECRETS_REPO"
   exit 1
@@ -17,23 +19,7 @@ fi
 echo "Installing $EXTERNAL_SECRETS_REPO on OpenShift Cluster"
 
 if [[ $EXTERNAL_SECRETS_REPO == "vault" ]]; then
-  # NOTE: Must have `oc`` installed and logged in with admin privileges
-  oc new-project hashicorp-vault
-  oc project hashicorp-vault
-  oc adm policy add-scc-to-user privileged \
-    system:serviceaccount:hashicorp-vault:vault-csi-provider
-
-  sed \
-    -e "s|VAULT_IMAGE_REPO|$VAULT_IMAGE_REPO|g" \
-    -e "s|VAULT_IMAGE_TAG|$VAULT_IMAGE_TAG|g" \
-    $SCRIPT_DIR/config/vault_values.yaml > $SCRIPT_DIR/config/tmp_vault_values.yaml
-
-  helm install \
-    -n hashicorp-vault vault hashicorp/vault \
-    --values $SCRIPT_DIR/config/tmp_vault_values.yaml
-
-  oc patch daemonset vault-csi-provider --type='json' \
-    -p='[{"op": "add", "path": "/spec/template/spec/containers/0/securityContext", "value": {"privileged": true} }]'
+  install_vault_openshift
 else
   echo "Did not recognize repo: $EXTERNAL_SECRETS_REPO"
   exit 1
