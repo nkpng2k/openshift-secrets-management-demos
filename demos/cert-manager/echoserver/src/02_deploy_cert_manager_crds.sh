6f46beafa5d15be7c4006991ef86deaa1af9136c
Nick Png <nkpng2k@gmail.com>
2025-09-17 12:13:58 -0400

    push up demo app yaml. Needs review

--- demos/cert-manager/echoserver/src/02_deploy_cert_manager_crds.sh
+++ demos/cert-manager/echoserver/src/02_deploy_cert_manager_crds.sh
@@ -7,8 +7,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 oc new-project cert-manager-demo-ns
 oc project cert-manager-demo-ns
 
+# Wait for cert-manager operator resources
+
+
 # Deploy cert-manager Issuer and Certificates
 oc apply -f $SCRIPT_DIR/config/cert_manager_crds.yaml
+
+# Wait for cert-manager secrets to become available
+
 
 # Verify server certificate against CA
 openssl verify -CAfile \
@@ -16,6 +22,9 @@ openssl verify -CAfile \
   <(oc get secret -n cert-manager-demo-ns test-server-tls -o jsonpath='{.data.ca\.crt}' | base64 -d)
 
 # Run simple test server via OpenSSL
+echo "Running simple OpenSSL Test"
+echo "-----"
+
 echo Hello World! > tmp_test.txt
 openssl s_server \
   -cert <(oc get secret -n cert-manager-demo-ns test-server-tls -o jsonpath='{.data.tls\.crt}' | base64 -d) \
@@ -32,5 +41,5 @@ echo -e "GET /tmp_test.txt HTTP/1.1\r\n\r\n" | \
     -CAfile <(oc get secret -n cert-manager-demo-ns test-client-tls -o jsonpath='{.data.ca\.crt}' | base64 -d) \
     -connect localhost:12345 -quiet
 
-kill -9 $SERVER_PID
+kill $SERVER_PID & wait $SERVER_PID 2>/dev/null
 rm tmp_test.txt
