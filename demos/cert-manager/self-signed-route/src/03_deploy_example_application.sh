46f827344f361f1b2ec37671b764d84ee977c802
Nick Png <nkpng2k@gmail.com>
2025-09-23 10:14:42 -0400

    Adds cert-manager demo for OpenShift Routes (#7)
    * make directories more descriptive, add scaffolding for routes demo
    
    * working routes demo
    
    * update readme

---
+++ demos/cert-manager/self-signed-route/src/03_deploy_example_application.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# source variables and util functions
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Add RBAC permissions for router to read referenced secrets
+oc create role secret-reader \
+  --verb=get,list,watch \
+  --resource=secrets \
+  --resource-name=test-server-tls
+
+oc create rolebinding secret-reader-binding \
+  --role=secret-reader \
+  --serviceaccount=openshift-ingress:router
+
+# Get OpenShift DNS name
+BASE_DOMAIN=$(oc get dns/cluster -o=jsonpath='{.spec.baseDomain}')
+APP_PREFIX=hello-openshift-route
+HOST=${APP_PREFIX}.apps.${BASE_DOMAIN}
+
+sed \
+  -e "s|DNS_HOST|$HOST|g" \
+  $SCRIPT_DIR/config/cert_manager_example.yaml > $SCRIPT_DIR/config/tmp_cert_manager_example.yaml
+oc apply -f $SCRIPT_DIR/config/tmp_cert_manager_example.yaml
+
+# Wait a bit for the deployment to be ready
+sleep 10
+
+# Run test with curl
+curl --cacert <(oc get secret -n cert-manager-demo-ns test-client-tls -o jsonpath='{.data.ca\.crt}' | base64 -d) \
+  -v https://$HOST
+
+# Sample test without --cacert should fail
+# curl -v https://$HOST
---
+++ demos/cert-manager/self-signed-route/src/04_cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clean up RBAC permissions
+oc delete rolebinding secret-reader-binding
+oc delete role secret-reader
+
+# Clean up demo namespace. This cleans up any resources in the namespace
+oc delete project cert-manager-demo-ns
+oc project default
+
+# Clean up operator group and subscription
+oc delete sub openshift-cert-manager-operator -n cert-manager-operator
+oc delete og openshift-cert-manager-operator -n cert-manager-operator
+CSV_NAME=$(oc get csv -n cert-manager-operator --no-headers | awk '{ print $1 }')
+oc delete csv -n cert-manager-operator $CSV_NAME
+
+# Clean up operator namespaces
+oc delete project cert-manager-operator
+oc delete project cert-manager
