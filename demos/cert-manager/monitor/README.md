fa404819e86349ad681e916cecc37f1fcf48742b
Nick Png <nkpng2k@gmail.com>
2026-01-26 09:33:44 -0500

    more rules changes

---
+++ demos/cert-manager/monitor/README.md
@@ -0,0 +1,9 @@
+# Monitoring Cert-Manager Resources
+
+```
+certmanager_certificate_not_after_timestamp_seconds{exported_namespace="cert-manager-monitor-ns"} - certmanager_certificate_renewal_timestamp_seconds{exported_namespace="cert-manager-monitor-ns"} # Time 
+```
+
+```
+certmanager_certificate_renewal_timestamp_seconds{exported_namespace="cert-manager-monitor-ns"} - time() # Time to renewal
+```
--- demos/cert-manager/monitor/src/03_deploy_monitoring_resources.sh
+++ demos/cert-manager/monitor/src/03_deploy_monitoring_resources.sh
@@ -4,3 +4,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 oc apply -f $SCRIPT_DIR/config/cluster-monitoring-config.yaml
 oc apply -f $SCRIPT_DIR/config/servicemonitor-cert-manager.yaml
+
+oc new-project cert-manager-monitor-ns
+
+BASE_DOMAIN=$(oc get dns/cluster -o=jsonpath='{.spec.baseDomain}')
+APP_PREFIX=hello-openshift-ingress
+HOST=${APP_PREFIX}.apps.${BASE_DOMAIN}
+sed \
+  -e "s|DNS_HOST|$HOST|g" \
+  $SCRIPT_DIR/config/resources-to-monitor.yaml > $SCRIPT_DIR/config/tmp-resources-to-monitor.yaml
+
+oc apply -f $SCRIPT_DIR/config/tmp-resources-to-monitor.yaml
