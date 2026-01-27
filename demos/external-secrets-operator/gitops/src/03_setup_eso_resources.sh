d59a72c296abede947ef2b32248abd5835579b10
Nick Png <nkpng2k@gmail.com>
2026-01-27 15:29:11 -0500

    pulls pem from vault; TODO: generate token from pem

--- demos/external-secrets-operator/gitops/src/03_setup_eso_resources.sh
+++ demos/external-secrets-operator/gitops/src/03_setup_eso_resources.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# source variables and util functions
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DEMO_SRC_DIR="external-secrets-operator/gitops/src"
+UTILS_DIR=$(sed "s|$DEMO_SRC_DIR|utils|g" <<< "$SCRIPT_DIR")
+source $UTILS_DIR/ocp.sh
+
+oc new-project eso-demo-ns
+oc project eso-demo-ns
+
+oc create serviceaccount -n eso-demo-ns eso-demo-sa
+
+IP_ADDRESS=$(oc get svc vault -n hashicorp-vault -o 'jsonpath={..spec.clusterIP}')
+
+sed \
+  -e "s|VAULT_SVC_CLUSTER_IP|$IP_ADDRESS|g" \
+  $SCRIPT_DIR/config/eso_resources.yaml > $SCRIPT_DIR/config/tmp_eso_resources.yaml
+oc apply -f $SCRIPT_DIR/config/tmp_eso_resources.yaml
